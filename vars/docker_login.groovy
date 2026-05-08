2a5ada4746710c90359b95a77f8e4261c5825cb0
Anil Choudhary <30832927+anil2211@users.noreply.github.com>
2026-05-08 16:38:44 +0530

    Add files via upload

---
+++ vars/docker_login.groovy
@@ -0,0 +1,13 @@
+def call(Map config = [:]) {
+    def credentials = config.credentials ?: 'dockerhub-creds'
+
+    withCredentials([usernamePassword(
+        credentialsId: credentials,
+        usernameVariable: 'DOCKER_USERNAME',
+        passwordVariable: 'DOCKER_PASSWORD'
+    )]) {
+        sh '''
+          echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+        '''
+    }
+}
